51ef9b491bfab74499a4efccd9f2154548ef34c3
Fabio Queiroz <fabiovianaqueiroz@gmail.com>
2020-10-04 18:20:35 +0100

    basic setup

---
+++ Middleware.Api/Controllers/RequestController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace Middleware.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RequestController : Controller
+    {
+        public RequestController()
+        {
+            
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Middleware.Api/RequestManager.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Middleware.Api
+{
+    public static class RequestManager
+    {
+        public static void RequestIdentifier(this IApplicationBuilder app, string requestType, Uri requestTargetUri, string sourceRequest, 
+            HttpMethod methodType, object model, string connectionString)
+        {
+
+        }
+    }
+}
